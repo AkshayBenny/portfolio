ade3d1bcfc00dd127efd4a94e96bdfe7a91f9442
Akshay Benny <akshaybenny@pop-os.localdomain>
2022-04-27 23:26:15 +0530

    update

--- webapp/client/src/App.js
+++ webapp/client/src/App.js
@@ -1,5 +1,66 @@
-function App() {
-  return <div>hello world</div>;
-}
+import React, { useState, useEffect } from 'react';
+import useSpeechToText from 'react-hook-speech-to-text';
 
-export default App;
+export default function App() {
+  const [data, setData] = useState([]);
+  const [voiceCommand, setVoiceCommand] = useState("")
+  const {
+    error,
+    interimResult,
+    isRecording,
+    results,
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false,
+  });
+
+  useEffect(() => {
+    fetch('http://localhost:8000/api/commands')
+      .then((res) => res.json())
+
+      .then((data) => setData(data));
+  }, []);
+
+  useEffect(() => {
+   
+    fetch('http://localhost:8000/api/commands', {
+      method: 'POST',
+      
+      // headers:{
+
+      // }
+    })
+      .then((res) => res.json())
+
+      .then((data) => setData(data));
+  }, [voiceCommand]);
+
+  console.log(data);
+  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+
+  return (
+    <div className='min-h-screen flex justify-center items-center bg-indigo-400'>
+      <div className='flex flex-col justify-center'>
+        <h1 className='uppercase text-3xl bold text-white text-center'>
+          Recording: {isRecording.toString()}
+        </h1>
+        <button
+          className='bg-white text-indigo-400 px-4 py-2 rounded border-white border-2 transition hover:bg-indigo-400 hover:text-white'
+          onClick={isRecording ? stopSpeechToText : startSpeechToText}
+        >
+          {isRecording ? 'Stop Recording' : 'Start Recording'}
+        </button>
+        <ul>
+          {results.map((result) => {
+            setVoiceCommand(result)
+            return (
+            <li  key={result.timestamp}>{result.transcript}</li>
+          )})}
+          {interimResult && <li>{interimResult}</li>}
+        </ul>
+      </div>
+    </div>
+  );
+}
