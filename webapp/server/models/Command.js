bb2829c63989b8e4bc63661e3141f0f87028c07b
Akshay Benny <akshaybenny@pop-os.localdomain>
2022-04-29 15:39:27 +0530

    models simplified

--- webapp/server/models/Command.js
+++ webapp/server/models/Command.js
@@ -2,14 +2,9 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const CommandSchema = new Schema(
-  {
-    id: { type: String },
-    command: { type: String, required: true },
-    read: { type: Boolean, required: true, default: false },
-  },
-
-  { timestamps: true }
-);
+const CommandSchema = new Schema({
+  command: { type: String, required: true },
+  read: { type: Boolean, required: true, default: false },
+});
 
 module.exports = mongoose.model('Command', CommandSchema);
