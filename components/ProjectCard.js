694f3dc9fc360e1aff14d1a2d3355fdc01efc8c6
Akshay Benny <akshaybennygh@gmail.com>
2022-07-13 15:54:15 +0530

    bug fix

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,6 +1,5 @@
 import { ArrowRightIcon } from '@heroicons/react/outline'
 
-
 const ProjectCard = ({
   number,
   image,
@@ -25,7 +24,9 @@ const ProjectCard = ({
         </p>
       </div>
       <div
-        className={`order-${number % 2 === 1 ? 1 : -1} flex flex-col justify-between pt-12 lg:pt-0`}
+        className={`order-${
+          number % 2 === 1 ? 1 : -1
+        } flex flex-col justify-between pt-12 lg:pt-0`}
       >
         <div className='flex flex-col space-y-[16px]'>
           <h1 className='font-semibold text-[40px] xl:text-[32px] tracking-[-1px] xl:tracking-[-0.5px]'>
@@ -48,7 +49,7 @@ const ProjectCard = ({
         <div className='flex items-center gap-12 font-semibold text-base pt-20 '>
           <div className='flex items-center gap-2 cursor-pointer group '>
             <a href={liveLink}>Live Demo</a>
-            <ArrowRightIcon className='h-6 group-hover:rotate-[-45deg] rotate-[-45deg] lg:rotate-[0deg]  transition bg-[#6068DB] rounded-full m-[6px] p-1' />
+            <ArrowRightIcon className='h-6 group-hover:rotate-[-45deg] rotate-[-45deg] lg:rotate-[0deg]  transition bg-[#6068DB] lg:bg-opacity-0 rounded-full m-[6px] p-1' />
           </div>
           <a href={gitLink}>Git Demo</a>
         </div>
