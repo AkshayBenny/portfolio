039776d096c3480f9a90c3008104da1331ba57bf
Akshay Benny <akshaybennyajh@gmail.com>
2023-07-17 14:01:29 +0530

    tagmanager added

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,24 @@ import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
 import { posthog } from 'posthog-js'
 import { useEffect } from 'react'
+import TagManager from 'react-gtm-module' // Google Tag Manager
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
+	useEffect(() => {
+		TagManager.initialize({ gtmId: 'GTM-MKKVSW5' })
 		const posthogToken = process.env.NEXT_PUBLIC_POSTHOG_TOKEN
 		posthogToken &&
 			posthog.init(posthogToken, {
 				api_host: 'https://app.posthog.com',
 			})
 	}, [])
-  return (
-    <ThemeProvider enableSystem={true} attribute='class'>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider
+			enableSystem={true}
+			attribute='class'>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
 }
 
 export default MyApp
