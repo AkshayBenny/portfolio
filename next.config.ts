51fca7f0e07ae8b09d3cc8e93c5b9bba595173bf
akshay <akshaybennygh@gmail.com>
2025-03-04 18:40:41 +0000

    edit

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
 						key: 'Content-Security-Policy',
 						value:
 							"default-src 'self'; " +
-							"script-src 'self' 'sha256-OBTN3RiyCV4Bq7dFqZ5a2pAXjnCcCYeTJMO2I/LYKeo='; " +
+							"script-src 'self' 'sha256-OBTN3RiyCV4Bq7dFqZ5a2pAXjnCcCYeTJMO2I/LYKeo=' https://vercel.live; " +
 							"style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
 							"img-src 'self' data:; " +
 							"font-src 'self' https://fonts.gstatic.com; " +
